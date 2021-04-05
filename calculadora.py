00358db3ad6d33a10fb82ab86a7a55a9609a7827
paula2454 <62024895+paula2454@users.noreply.github.com>
2021-04-05 20:04:21 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,5 @@
 # Calculadora em Python
-
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos 2 e 3. 
-# A solução será apresentada no próximo capítulo!
-# Assista o vídeo com a execução do programa!
+#Codigo atualizado com as clausulas de erro e exceção
 
 print("\n******************* Python Calculator *******************\n")
 print("Selecione o número da operação desejada:\n")
@@ -11,19 +8,29 @@ print("2 = Subtração")
 print("3 = Multiplicação")
 print("4 = Divisão")
 
-operacao = int(input("\nDigite sua opção (1/2/3/4): "))
-
-num1 = float(input("\nDigite o primeiro número: "))
-num2 = float(input("\nDigite o segundo número: "))
-
-if operacao == 1:
-	print(num1, "+", num2,"=", num1 + num2)
-elif operacao == 2:
-	print(num1, "-", num2,"=",num1 - num2)
-elif operacao == 3:
-	print(num1, "*", num2,"=",num1 * num2)
-elif operacao == 4:
-	print(num1, "/", num2,"=",num1/num2)
-else:
-	print ("Operação Invalida!")
+while True:
+	try:
+		operacao = int(input("\nDigite sua opção (1/2/3/4): "))
+		num1 = float(input("\nDigite o primeiro número: "))
+		num2 = float(input("\nDigite o segundo número: "))
+		
+		if operacao == 1:
+			print(num1, "+", num2,"=", num1 + num2)
+		elif operacao == 2:
+			print(num1, "-", num2,"=",num1 - num2)
+		elif operacao == 3:
+			print(num1, "*", num2,"=",num1 * num2)
+		elif operacao == 4:
+			print(num1, "/", num2,"=",num1/num2)
+			
+	
+	except:
+		print("Operação Inválida! Tente novamente.")
+		continue
+	else:
+		if operacao not in (1,2,3,4):
+			print("Opção Invalida! Digite uma opção valida dentre as abaixo:")
+			continue
+		else:
+			break
 
